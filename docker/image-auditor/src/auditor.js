2cf727ba78a85a760af9214e2cfa31e26b3b7883
emerzit <jeremyfou@hotmail.com>
2020-06-27 17:28:37 +0200

    finished the operational part

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -14,24 +14,76 @@
  */
 const protocol = require('./music-protocol');
 
-/*s
+/*
  * We use a standard Node.js module to work with UDP
  */
 const dgram = require('dgram');
+/*
+ * We use a standard Node.js module to work with TCP
+ */
+const net = require('net');
+
+// let's create a TCP server
+const server = net.createServer();
+
+const moment = require('moment');
+
+server.on('connection',
+    congolexicomatisationDesLoisDuMarche);
+//Start listening on port 9907
+server.listen(2205);
 
 /*
  * Let's create a datagram socket. We will use it to listen for datagrams published in the
  * multicast group by musician and containing sound
  */
 const s = dgram.createSocket('udp4');
-s.bind(protocol.PROTOCOL_PORT, function() {
+s.bind(protocol.PROTOCOL_PORT, function () {
     console.log("Joining multicast group");
     s.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
 });
 
+function cleanup(mapyy) {
+    musiciansList.forEach(function (value, key) {
+        if (moment().subtract(value.moment).format("X") > 5) {
+            musiciansList.delete(key);
+        }
+    });
+    console.log(musiciansList);
+}
+
+let musiciansList = new Map();
+
+function congolexicomatisationDesLoisDuMarche(socket) {
+    let jsonSend = [];
+    cleanup(musiciansList);
+    musiciansList.forEach(function (value, key) {
+        jsonSend.push({uuid: key, instrument: value.instrument, activeSince: value.createdAt.format()})
+    });
+    socket.write(JSON.stringify(jsonSend));
+    socket.end();
+}
+
 /*
  * This call back is invoked when a new datagram has arrived.
  */
-s.on('message', function(msg, source) {
+s.on('message', function (msg, source) {
     console.log("Data has arrived: " + msg + ". Source port: " + source.port);
+    let jsonData = JSON.parse(msg);
+    let instrument = undefined;
+    for (instr in protocol.INSTRUMENTS) {
+        if (protocol.INSTRUMENTS[instr] === jsonData.sound) {
+            instrument = instr;
+        }
+    }
+    if (musiciansList.has(jsonData.uuid)) {
+        musiciansList.set(jsonData.uuid, {
+            instrument: instrument,
+            moment: moment(),
+            createdAt: musiciansList.get(jsonData.uuid).createdAt
+        });
+    } else {
+        musiciansList.set(jsonData.uuid, {instrument: instrument, moment: moment(), createdAt: moment()});
+    }
+    cleanup(musiciansList);
 });
